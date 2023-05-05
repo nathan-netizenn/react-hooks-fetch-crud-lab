439a3ba900fdb63de74f3f9856510d18ac38e99b
nathan <nathanmageka6@gmail.com>
2023-05-06 00:02:17 +0300

    nathan

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete, onUpdateCorrectAnswer }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,26 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  const handleDelete = () => {
+    onDelete(id);
+  };
+
+  const handleSelectChange = (e) => {
+    const newCorrectIndex = parseInt(e.target.value);
+    onUpdateCorrectAnswer(id, newCorrectIndex);
+  };
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleSelectChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import QuestionItem from './QuestionItem';
 
-function QuestionList() {
+const QuestionList = () => {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Fetch questions from API on component mount
+    const fetchQuestions = async () => {
+      try {
+        const response = await fetch('http://localhost:4000/questions');
+        const data = await response.json();
+        setQuestions(data);
+      } catch (error) {
+        console.error('Error fetching questions:', error);
+      }
+    };
+    fetchQuestions();
+  }, []);
+
+  const handleDeleteQuestion = async (id) => {
+    // Delete question from API and update state
+    try {
+      await fetch(`http://localhost:4000/questions/${id}`, {
+        method: 'DELETE'
+      });
+      setQuestions(prevQuestions =>
+        prevQuestions.filter(question => question.id !== id)
+      );
+    } catch (error) {
+      console.error('Error deleting question:', error);
+    }
+  };
+
+  const handleUpdateCorrectAnswer = async (id, correctIndex) => {
+    // Update correct answer of question in API and update state
+    try {
+      await fetch(`http://localhost:4000/questions/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ correctIndex })
+      });
+      setQuestions(prevQuestions =>
+        prevQuestions.map(question => {
+          if (question.id === id) {
+            return { ...question, correctIndex };
+          } else {
+            return question;
+          }
+        })
+      );
+    } catch (error) {
+      console.error('Error updating correct answer:', error);
+    }
+  };
+
   return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+    <div>
+      <h2>Questions</h2>
+      {questions.map(question => (
+        <QuestionItem
+          key={question.id}
+          question={question}
+          onDelete={handleDeleteQuestion}
+          onUpdateCorrectAnswer={handleUpdateCorrectAnswer}
+        />
+      ))}
+    </div>
   );
-}
-
+};
 export default QuestionList;
